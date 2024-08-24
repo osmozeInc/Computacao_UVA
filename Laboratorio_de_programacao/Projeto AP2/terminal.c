b526e7180f54732040fb06813c565819a54ac7b4
Caio Monte <caiomm076@gmail.com>
2024-08-24 15:30:26 -0300

    direcionamento para arquivo windowns

---
+++ Laboratorio_de_programacao/Projeto AP2/terminal.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Função para escolher o sistema
+int Systema()
+{
+    int sistema;
+
+    printf("Qual seu sistema?\n");
+    printf("1 - Windows\n");
+    printf("2 - Linux ou Mac\n");
+    printf("3 - Sair\n");
+
+    scanf("%d", &sistema);
+    return sistema;
+}
+
+int main() {
+    system("cls || clear");
+
+    int sistema = Systema();
+
+    if (sistema == 1) {
+        system("cd Proj_windows && gcc main.c");  // apagar após terminar o código
+        system("cd Proj_windows && main.exe");
+
+    } else if (sistema == 2) {
+        system("cd ~/Proj_linux_mac && ./main");
+
+    } else if (sistema == 3) {
+        printf("Saindo...\n");
+        return 0;
+
+    } else {
+        printf("Opção inválida!\n");
+    }
+
+    return 0;
+}
