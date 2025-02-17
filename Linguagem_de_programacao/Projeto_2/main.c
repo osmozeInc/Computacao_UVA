22551548c9a5c0cf53d853ed72c67cabd242032a
Caio Monte <caiomm076@gmail.com>
2025-02-16 22:19:01 -0300

    projeto - começo

---
+++ Linguagem_de_programacao/Projeto_2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+
+
+
+int main(){
+    
+
+    // tela de inicio
+    // decidir forma de jogo
+
+    // 1 jogo comum
+    // 1 definição das variaveis do jogo
+    // 1 inicio da tela de jogo
+    // 1 fim do jogo $
+
+    // 2 jogo personalizado
+    // 2 definição das variaveis do jogo pelo usuário
+    // 2 inicio da tela de jogo personalizado
+    // 2 fim do jogo $
+
+    // mensagem final de fim de jogo
+}
